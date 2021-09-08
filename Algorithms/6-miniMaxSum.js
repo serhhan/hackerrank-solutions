6ccb2bc1a5f6e4e77e13075ada6f804979600730
serhanelmali <serhanelmali@gmail.com>
2021-09-08 22:46:23 +0300

    refactor Mini-Max Sum

--- Algorithms/6-miniMaxSum.js
+++ Algorithms/6-miniMaxSum.js
@@ -1,13 +1,23 @@
 function miniMaxSum(arr) {
-  const summedNumbers = arr.map((number) => {
-    let sum = 0;
-    arr.forEach((num) => {
-      num != number && (sum += num);
-    });
-    return sum;
-  });
-
-  summedNumbers.sort((a, b) => a - b);
+  const sortedNumbers = arr.sort((a, b) => b - a);
+  const summedNumbers =
+    sortedNumbers[0] == sortedNumbers[arr.length - 1]
+      ? sortedNumbers.map((number) => {
+          let sum = 0;
+          sortedNumbers.slice(0, sortedNumbers.length - 1).forEach((num) => {
+            sum += num;
+          });
+          return sum;
+        })
+      : sortedNumbers.map((number) => {
+          let sum = 0;
+          sortedNumbers.forEach((num) => {
+            num !== number && (sum += num);
+          });
+          return sum;
+        });
 
   console.log(summedNumbers[0], summedNumbers[summedNumbers.length - 1]);
 }
+
+miniMaxSum([1, 2, 3, 4, 5]);
