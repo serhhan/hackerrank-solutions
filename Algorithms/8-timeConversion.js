a30bc8e10f6942cc19825703ffcfc22ddd55da03
Serhan Elmalı <serhanelmali@gmail.com>
2021-11-18 23:33:14 +0300

    solution of Jumping On the Clouds

--- Algorithms/8-timeConversion.js
+++ Algorithms/8-timeConversion.js
@@ -1,5 +1,6 @@
 function timeConversion(s) {
   const arr = s.slice(0, 8).split(":");
+
   arr[0] = s.includes("PM")
     ? arr[0] == 12
       ? "12"
---
+++ Algorithms/9-jumpingOnClouds.js
@@ -0,0 +1,13 @@
+function jumpingOnClouds(c) {
+  let minimumJump = 0;
+
+  for (let i = 0; i < c.length + 1; i += 2) {
+    if (c[i] === 1) {
+      i -= 1;
+    }
+    minimumJump++;
+  }
+
+  minimumJump--;
+  return minimumJump;
+}
