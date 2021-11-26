334b826ea179b4c8473851d0d3df426f542d5ed3
Serhan Elmalı <serhanelmali@gmail.com>
2021-11-26 23:07:54 +0300

    adds solution of Picking Numbers

---
+++ Algorithms/23-pickingNumbers.js
@@ -0,0 +1,26 @@
+function pickingNumbers(a) {
+  let array = a.sort((a, b) => a - b);
+
+  let length = 0;
+  while (array.length > 0) {
+    let n = array[0];
+    array.shift();
+    let temp = 1;
+
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === n || array[j] === n - 1 || array[j] === n + 1) {
+        temp++;
+      } else {
+        break;
+      }
+
+      if (temp > length) {
+        length = temp;
+      }
+    }
+  }
+
+  return length;
+}
+
+pickingNumbers([8, -1, 0, 4, 3, 210, 21]);
