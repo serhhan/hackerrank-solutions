a1cf2aee0af9d2291b009bffd1e0407f4a1cbae9
Serhan Elmalı <serhanelmali@gmail.com>
2021-11-30 02:16:24 +0300

    adds solution of designerPDFviewer

---
+++ Algorithms/26-designerPDFviewer.js
@@ -0,0 +1,41 @@
+function designerPdfViewer(h, word) {
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  let wordHeights = [],
+    tallestValue = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    let index = alphabet.indexOf(word[i]);
+    let = wordHeights.push(h[index]);
+  }
+
+  tallestValue = Math.max(...wordHeights);
+  return word.length * tallestValue;
+}
