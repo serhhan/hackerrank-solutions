3b3b369ed350193303f4093e4b5bb3c9eff9940c
Serhan Elmalı <serhanelmali@gmail.com>
2021-12-03 23:21:31 +0300

    refactors Jumping On Clouds

--- Algorithms/9-jumpingOnTheClouds.js
+++ Algorithms/9-jumpingOnTheClouds.js
@@ -1,13 +1,12 @@
 function jumpingOnClouds(c) {
   let minimumJump = 0;
 
-  for (let i = 0; i < c.length + 1; i += 2) {
+  for (let i = 0; i < c.length - 1; i += 2) {
     if (c[i] === 1) {
       i -= 1;
     }
     minimumJump++;
   }
 
-  minimumJump--;
   return minimumJump;
 }
